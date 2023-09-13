379a85ddc68b547e1cb8678d67242d1a73bf331f
NolaD25 <noladeroo25@marlborough.org>
2023-09-13 09:33:47 -0700

    added multiples of 3

---
+++ unit0/Sequences.java
@@ -0,0 +1,41 @@
+public class Sequences
+{
+    public static void main (String[] args)
+    {
+        int n = 5;
+        System.out.println(n);
+        
+        n =23;
+        System.out.println(n);
+        
+        float x = 1.23f;
+        System.out.println(x);
+        
+        double y = 2.34;
+        System.out.println(y);
+        
+        boolean happy = true;
+        System.out.println(happy);
+        
+        String name = "Nola";
+        System.out.println(name);
+        
+        //print odd numbers
+        
+        for(int i=0; i<10; i++)
+        {
+           if(i%2==1)
+               System.out.println(i);
+            
+        }
+        
+        int value = 1;
+        for(int i =0; i<5; i++)
+        {
+            System.out.println(value);
+            value += 2; // value = value + 2
+            
+        }
+        
+    }
+}
