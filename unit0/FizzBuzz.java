923256cf0d1a53317ca73a97e79b134f6ace8d9b
NolaD25 <noladeroo25@marlborough.org>
2023-09-13 09:34:43 -0700

    Update FizzBuzz.java

--- unit0/FizzBuzz.java
+++ unit0/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz
     public static void main (String[] args)
     {
         
-        for(int i = 0;i < 31; i++)
+        for(int i = 0;i < 30; i++)
             {
                 if(i%3 == 0 && i%5 == 0)
                     System.out.println("FizzBuzz");
