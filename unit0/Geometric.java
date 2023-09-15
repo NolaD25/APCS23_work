9d94b2d18cca57ed8b8aa453b7d44a76ae755de2
NolaD25 <noladeroo25@marlborough.org>
2023-09-14 19:35:21 -0700

    Added Geometric sequence

---
+++ unit0/Geometric.java
@@ -0,0 +1,12 @@
+public class Geometric
+{
+    public static void main (String[] args)
+    {
+        
+        int x = 2;
+        for(int i =0; i < 10; i++)
+            System.out.println(i*x);
+         
+    }
+    
+}
