3bd1c4f440a427be633eca6837e579ccf7a7b552
NolaD25 <noladeroo25@marlborough.org>
2023-09-18 14:47:09 -0700

    moved assignments

--- unit1/Fibonacci.java
+++ unit1/Fibonacci.java
@@ -2,10 +2,9 @@ public class Fibonacci
 {
     public static void main (String[] args)
     {
-        int n = 30;
         int a = 0;
         int b = 1;
-        for(int i = 0; i < n;i++)
+        for(int i = 0; i < 30;i++)
         {
             System.out.println(a);
             int next = a+b;
